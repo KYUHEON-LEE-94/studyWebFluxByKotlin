5bea502fcddc190fe06752b2afe23b36ce796355
Maizurzu <panpan68@naver.com>
2024-01-20 23:55:34 +0900

    SchedulersTest

--- src/test/kotlin/com/study/webflux/operationTest/SchedulersTest.kt
+++ src/test/kotlin/com/study/webflux/operationTest/SchedulersTest.kt
@@ -3,9 +3,14 @@ package com.study.webflux.operationTest
 import org.junit.jupiter.api.Test
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
 import reactor.test.StepVerifier
 import reactor.test.scheduler.VirtualTimeScheduler
 import java.time.Duration
+import java.util.concurrent.Executor
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
 /*operation 이어서*/
 class SchedulersTest {
 
@@ -33,8 +38,59 @@ class SchedulersTest {
         Thread.sleep(2000)
     }
 
-    private fun log(operateName: String, data:String?){
+    @Test
+    fun schedulerExample() {
+        Mono.just("1").log()
+            .publishOn(Schedulers.fromExecutorService(executorService()))
+            .map{
+                log("map", it)
+                it.toInt()
+            }
+            .subscribe(::println)
+    }
+
+    @Test
+    fun subscribeOnExample() {
+        Mono.just("1").log()
+            .subscribeOn(Schedulers.parallel())
+            .map{
+                log("map1", it)
+                it.toInt()
+            }
+            .subscribeOn(Schedulers.boundedElastic())
+            .subscribeOn(Schedulers.boundedElastic())
+            .map{
+                log("map2", it)
+                it*2
+            }
+
+            .subscribe(::println)
+
+        Thread.sleep(1000)
+    }
+
+    @Test
+    fun runOnExample() {
+
+        println("Number of CPU cores ${Runtime.getRuntime().availableProcessors()}")
+        Flux.range(1, 20)
+            .parallel()
+            .runOn(Schedulers.parallel())
+            .map{
+                log("map1", it)
+                it.toInt()
+            }
+            .subscribe(::println)
+
+        Thread.sleep(1000)
+    }
+
+    private fun executorService(): ExecutorService = Executors.newFixedThreadPool(10)
+
+    /*log which Tread*/
+    private fun <T>log(operateName: String, data:T?){
         println("Inside operator $operateName, data is $data, Thread : ${Thread.currentThread().name}")
     }
+
 
 }
